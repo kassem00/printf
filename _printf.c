d6f8d9636babeb64c08ba2db3b277a03d7a3cc60
kassem00 <kamsdonga@gmail.com>
2023-03-29 04:54:45 +0200

    1

--- _printf.c
+++ _printf.c
@@ -9,37 +9,51 @@
 
 int _printf(const char *format, ...)
 {
-va_list args;
-int count = 0;
-char c, *s;
-va_start(args, format);
-while (*format != '\0') {
-if (*format == '%') {
-format++;
-switch (*format) {
-case 'c':
-c = va_arg(args, int);
-count += putchar(c);
-break;
-case 's':
-s = va_arg(args, char *);
-count += printf("%s", s);
-break;
-case '%':
-count += putchar('%');
-break;
-default:
-count += putchar('%');
-count += putchar(*format);
-break;
-}
-}
-else
-{
-count += putchar(*format);
-}
-format++;
-}
-va_end(args);
-return count;
+    va_list args;
+    int count = 0;
+
+    va_start(args, format);
+
+    while (*format != '\0') {
+        if (*format == '%') {
+            format++;
+            switch (*format) {
+                case 'c': {
+                    char c = va_arg(args, int);
+                    putchar(c);
+                    count++;
+                    break;
+                }
+                case 's': {
+                    char *s = va_arg(args, char *);
+                    int n = 0;
+                    while (s[n] != '\0') {
+                        putchar(s[n]);
+                        n++;
+                        count++;
+                    }
+                    break;
+                }
+                case '%': {
+                    putchar('%');
+                    count++;
+                    break;
+                }
+                default: {
+                    putchar('%');
+                    putchar(*format);
+                    count += 2;
+                    break;
+                }
+            }
+        } else {
+            putchar(*format);
+            count++;
+        }
+        format++;
+    }
+
+    va_end(args);
+
+    return count;
 }
