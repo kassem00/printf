ba13d0d1f22395a50b50a7509bd4e779bb026169
kassem00 <kamsdonga@gmail.com>
2023-03-29 02:46:18 +0200

    1

--- _printf.c
+++ _printf.c
@@ -3,55 +3,48 @@
 /**
  * _printf - prints output according to a format
  * @format: format string
- *
+ * @...: input
  * Return: number of characters printed
  */
 int _printf(const char *format, ...)
 {
-    int count = 0;
-    va_list args;
+int count = 0;
+va_list args;
 
-    va_start(args, format);
+va_start(args, format);
 
-    while (*format)
-    {
-        if (*format == '%')
-        {
-            format++;
-
-            switch (*format)
-            {
-                case 'c':
-                    putchar(va_arg(args, int));
-                    count++;
-                    break;
-
-                case 's':
-                    count += fputs(va_arg(args, const char *), stdout);
-                    break;
-
-                case '%':
-                    putchar('%');
-                    count++;
-                    break;
-
-                default:
-                    putchar('%');
-                    putchar(*format);
-                    count += 2;
-                    break;
-            }
-        }
-        else
-        {
-            putchar(*format);
-            count++;
-        }
-
-        format++;
-    }
-
-    va_end(args);
-
-    return count;
+while (*format)
+{
+if (*format == '%')
+{
+format++;
+switch (*format)
+{
+case 'c':
+putchar(va_arg(args, int));
+count++;
+break;
+case 's':
+count += fputs(va_arg(args, const char *), stdout);
+break;
+case '%':
+putchar('%');
+count++;
+break;
+default:
+putchar('%');
+putchar(*format);
+count += 2;
+break;
+}
+}
+else
+{
+putchar(*format);
+count++;
+}
+format++;
+}
+va_end(args);
+return count;
 }
