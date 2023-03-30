62baa58382789985126f3fa99dbbc2cb407638e0
kassem00 <kamsdonga@gmail.com>
2023-03-30 05:21:14 +0200

    1

--- _printf.c
+++ _printf.c
@@ -10,59 +10,54 @@
  */
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int length = 0;
-
-	va_start(args, format);
-
-	while (*format != '\0')
-	{
-		if (*format == '%')
-		{
-			format++;
-			switch (*format)
-			{
-				case 'c':
-					length += printf("%c", va_arg(args, int));
-					break;
-				case 's':
-					length += printf("%s", va_arg(args, char *));
-					break;
-				case '%':
-					length += printf("%%");
-					break;
-				case 'd':
-				case 'i':
-					length += printf("%d", va_arg(args, int));
-					break;
-				case 'u':
-					length += printf("%u", va_arg(args, unsigned int));
-					break;
-				case 'o':
-					length += printf("%o", va_arg(args, unsigned int));
-					break;
-				case 'x':
-					length += printf("%x", va_arg(args, unsigned int));
-					break;
-				case 'X':
-					length += printf("%X", va_arg(args, unsigned int));
-					break;
-				case 'p':
-					length += printf("%p", va_arg(args, void *));
-					break;
-				default:
-					length += printf("%%%c", *format);
-			}
-		}
-		else
-		{
-			length += putchar(*format);
-		}
-
-		format++;
-	}
-
-	va_end(args);
-
-	return length;
+va_list args;
+int length = 0;
+va_start(args, format);
+while (*format != '\0')
+{
+if (*format == '%')
+{
+format++;
+switch (*format)
+{
+case 'c':
+length += printf("%c", va_arg(args, int));
+break;
+case 's':
+length += printf("%s", va_arg(args, char *));
+break;
+case '%':
+length += printf("%%");
+break;
+case 'd':
+case 'i':
+length += printf("%d", va_arg(args, int));
+break;
+case 'u':
+length += printf("%u", va_arg(args, unsigned int));
+break;
+case 'o':
+length += printf("%o", va_arg(args, unsigned int));
+break;
+case 'x':
+length += printf("%x", va_arg(args, unsigned int));
+break;
+case 'X':
+length += printf("%X", va_arg(args, unsigned int));
+break;
+case 'p':
+length += printf("%p", va_arg(args, void *));
+break;
+default:
+length += printf("%%%c", *format);
+}
+}
+else
+{
+length += putchar(*format);
+}
+format++;
+}
+va_end(args);
+return length;
 }
