695e1efcdce1af8549beeda4db6e48490348842e
kassem00 <kamsdonga@gmail.com>
2023-03-30 05:12:58 +0200

    1

--- _printf.c
+++ _printf.c
@@ -1,69 +1,45 @@
 #include "main.h"
-
-/**
- * _printf - prints output according to a format.
- * @format: A pointer to a string to be printed.
- *
- * Return: The number of characters printed (excluding the null byte used to end output to strings).
- */
 int _printf(const char *format, ...)
 {
-int count = 0;
-va_list args;
-int i;
-char *str;
-va_start(args, format);
-for (i = 0; format && format[i]; i++)
-{
-if (format[i] == '%')
-{
-switch (format[++i])
-{
-case 'c':
-putchar(va_arg(args, int));
-count++;
-break;
-case 's':
-str = va_arg(args, char *);
-if (!str)
-str = "(null)";
-incress_print(&count, str, constchartype);
-break;
-case '%':
-putchar('%');
-count++;
-break;
-default:
-putchar('%');
-putchar(format[i]);
-count += 2;
-break;
-}
-}
-else
-{
-putchar(format[i]);
-count++;
-}
-}
-va_end(args);
-return (count);
-}
-void incress_print(int *count, ...)
-{
-va_list args;
-va_start(args, count);
-while (*count != INT_MAX)
-{
-if (va_arg(args, int) == chartype)
-putchar(va_arg(args, int));
-else if (va_arg(args, int) == constchartype)
-fputs(va_arg(args, const char *), stdout);
-else if (va_arg(args, int) == inttype)
-printf("%d", va_arg(args, int));
-else
-break;
-(*count)++;
-}
-va_end(args);
+	va_list args;
+	char c, *s;
+	int n = 0;
+
+	va_start(args, format);
+
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			switch (*format)
+			{
+				case 'c':
+					c = (char) va_arg(args, int);
+					n += write(1, &c, 1);
+					break;
+				case 's':
+					s = va_arg(args, char *);
+					if (s == NULL)
+						s = "(null)";
+					n += write(1, s, strlen(s));
+					break;
+				case '%':
+					n += write(1, "%", 1);
+					break;
+				default:
+					n += write(1, "%", 1);
+					n += write(1, &(*format), 1);
+					break;
+			}
+		}
+		else
+		{
+			n += write(1, &(*format), 1);
+		}
+		format++;
+	}
+
+	va_end(args);
+	return (n);
 }
