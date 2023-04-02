6153110951a1cf3ae8b1b481a03c0ed5022a3d2d
kassem00 <kamsdonga@gmail.com>
2023-04-02 07:15:43 +0200

    1

--- _printf.c
+++ _printf.c
@@ -32,7 +32,8 @@ width = parse_width(format, &i, list);
 precision = parse_precision(format, &i, list);
 size = parse_size(format, &i);
 ++i;
-printed = parse_handle(format, &i, list, buffer, flags, width, precision, size);
+printed = parse_handle(format, &i, list, buffer, \
+flags, width, precision, size);
 if (printed == -1)
 return (-1);
 printed_chars += printed;
